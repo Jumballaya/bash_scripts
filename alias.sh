e453ff7e48682e3129f607d3868fd6b5b153730f
Patrick Burris <patrick@briefmedia.com>
2017-08-06 21:57:30 -0500

    add aliases

---
+++ alias.sh
@@ -0,0 +1,10 @@
+export PS1="\W\$(git-radar --bash --fetch) "
+
+# ALIAS
+alias reload='source ~/.zshrc'
+alias repo='sh ~/bin/repo.sh'
+alias ccomp='sh ~/bin/ccomp.sh'
+alias initialize='sh ~/bin/starter.sh'
+alias dkill='sh ~/bin/dkill.sh'
+alias http='sh ~/bin/http.sh'
+alias freakout='sh ~/bin/lefreak.sh'
