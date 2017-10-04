64eaefda29f6f1d353d1366b9b268a7a45a78742
Patrick Burris <patrick@briefmedia.com>
2017-10-04 08:47:31 -0500

    update docker tools

--- dkill.sh
+++ dkill.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-# Removes all running containers and then removes the old containers
-# Removes containers made months, days and weeks ago
-docker ps -q | xargs docker kill
-docker ps -a -q | xargs docker rm
+function kill_containers {
+  docker ps -q | xargs docker kill
+  docker ps -a -q | xargs docker rm
+}
+
+function remove_images {
+  docker rmi $(docker images -f dangling=true -q)
+}
+
+function remove_volumes {
+  docker volume ls -q | xargs docker volume rm
+}
+
+if [ "$1" = "" ]; then
+  kill_containers
+  remove_images
+  remove_volumes
+fi
+
+if [ "$1" = "-v" ]; then
+  remove_volumes
+fi
+
+if [ "$1" = "-i" ]; then
+  remove_volumes
+fi
+
+if [ "$1" = "-c" ]; then
+  kill_containers
+fi
